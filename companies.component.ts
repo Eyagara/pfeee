18aa69f3550257caefe2b365dc95808687eb71d9
Eya Gara <158833290+Eyagara@users.noreply.github.com>
2024-04-14 22:53:40 +0200

    Add files via upload

---
+++ companies.component.ts
@@ -0,0 +1,22 @@
+// companies.component.ts
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.scss']
+})
+export class CompaniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialisation
+  }
+
+  // Méthodes et logique de gestion des entreprises
+}
+
+
+
---
+++ companies.module.ts
@@ -0,0 +1,18 @@
+// companies.module.ts
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CompaniesComponent } from './companies.component';
+
+@NgModule({
+  declarations: [CompaniesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CompaniesModule { }
+
+
+
+
+
